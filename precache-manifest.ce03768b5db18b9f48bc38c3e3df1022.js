e95a0629ac0008324c88ae5d950c02a7ff5306c2
Karthik Arumugam <chivalrykarthik@gmail.com>
2019-10-15 21:33:59 +0530

    Updates

---
+++ precache-manifest.ce03768b5db18b9f48bc38c3e3df1022.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7686a04c096a9ccabc9d18c02d320059",
+    "url": "/react-list-app/index.html"
+  },
+  {
+    "revision": "d6d9050e6023339b2678",
+    "url": "/react-list-app/static/css/2.a62317bb.chunk.css"
+  },
+  {
+    "revision": "943b8c4cdf5e69f94a61",
+    "url": "/react-list-app/static/css/main.13e91b60.chunk.css"
+  },
+  {
+    "revision": "d6d9050e6023339b2678",
+    "url": "/react-list-app/static/js/2.6de66b44.chunk.js"
+  },
+  {
+    "revision": "943b8c4cdf5e69f94a61",
+    "url": "/react-list-app/static/js/main.4ad3d6cf.chunk.js"
+  },
+  {
+    "revision": "61501b981f7c0a2bdcdc",
+    "url": "/react-list-app/static/js/runtime-main.380d2679.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-list-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-list-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-list-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-list-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-list-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
